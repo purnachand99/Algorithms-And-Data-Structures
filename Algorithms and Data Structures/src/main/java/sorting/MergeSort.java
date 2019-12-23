5675721de3b044bebf41681a38f7bcea365e0985
purnachand99 <purnachand99@gmail.com>
2019-12-23 10:24:40 +0530

    merge sort merge method updated

--- Algorithms and Data Structures/src/main/java/sorting/MergeSort.java
+++ Algorithms and Data Structures/src/main/java/sorting/MergeSort.java
@@ -9,20 +9,66 @@ public class MergeSort {
 			mergeSort(Array, m + 1, right);
 			merge(Array, left, m, right);
 		}
-	}//end of method
+	}// end of method
 
-	
-	static void merge(int[] A, int left, int middle, int right) {
-		int [] leftTmpArray = new int[middle-left+2];  //Create tmp arrays
-		int [] rightTmpArray = new int[right-middle+1];
-		
-		for(int i=0;i<=middle-left;i++) //Copy values from Array 'A' to these tmp arrays
-			leftTmpArray[i]= A[left+i];
-		for(int i=0;i<right-middle;i++)
-			rightTmpArray[i]= A[middle+1+i];
-		
-		leftTmpArray[middle-left+1]= Integer.MAX_VALUE; //Merge values and insert into Array 'A'
-		rightTmpArray[right-middle] = Integer.MAX_VALUE;
+	static void merge(int arr[], int l, int m, int r) {
+		// Find sizes of two subarrays to be merged
+		int n1 = m - l + 1;
+		int n2 = r - m;
+
+		/* Create temp arrays */
+		int L[] = new int[n1];
+		int R[] = new int[n2];
+
+		/* Copy data to temp arrays */
+		for (int i = 0; i < n1; ++i)
+			L[i] = arr[l + i];
+		for (int j = 0; j < n2; ++j)
+			R[j] = arr[m + 1 + j];
+
+		/* Merge the temp arrays */
+		// Initial indexes of first and second subarrays
+		int i = 0, j = 0;
+
+		// Initial index of merged subarry array
+		int k = l;
+		while (i < n1 && j < n2) {
+			if (L[i] <= R[j]) {
+				arr[k] = L[i];
+				i++;
+			} else {
+				arr[k] = R[j];
+				j++;
+			}
+			k++;
+		}
+
+		/* Copy remaining elements of L[] if any */
+		while (i < n1) {
+			arr[k] = L[i];
+			i++;
+			k++;
+		}
+
+		/* Copy remaining elements of R[] if any */
+		while (j < n2) {
+			arr[k] = R[j];
+			j++;
+			k++;
+		}
+	}
+
+	static void merge_old(int[] A, int left, int middle, int right) {
+		int[] leftTmpArray = new int[middle - left + 2]; // Create tmp arrays
+		int[] rightTmpArray = new int[right - middle + 1];
+
+		for (int i = 0; i <= middle - left; i++) // Copy values from Array 'A' to these tmp arrays
+			leftTmpArray[i] = A[left + i];
+		for (int i = 0; i < right - middle; i++)
+			rightTmpArray[i] = A[middle + 1 + i];
+
+		leftTmpArray[middle - left + 1] = Integer.MAX_VALUE; // Merge values and insert into Array 'A'
+		rightTmpArray[right - middle] = Integer.MAX_VALUE;
 		int i = 0, j = 0;
 		for (int k = left; k <= right; k++) {
 			if (leftTmpArray[i] < rightTmpArray[j]) {
@@ -33,13 +79,12 @@ public class MergeSort {
 				j++;
 			}
 		}
-	}//end of method
-	
-	
-	public static void printArray(int []array) {
-		for (int i = 0; i < array.length; i++) {
-			System.out.print(array[i]+"  ");
-		}
-	}//end of method
+	}// end of method
 
-}//end of class
+	public static void printArray(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + "  ");
+		}
+	}// end of method
+
+}// end of class
